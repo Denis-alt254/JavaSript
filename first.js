4eff6325af89e0a81a7da41bee55bbba7ca3d963
Denis Kipkurui <deniskipkurui2024@gmail.com>
2025-03-16 12:53:26 +0300

    first

---
+++ first.js
@@ -0,0 +1,13 @@
+let y = 4;
+var t = 56;
+
+function myfunction(){
+    if (t > y){
+        console.log(" T is greater than y.")
+    }
+    else{
+        console.log("y is greater than t.")
+    }
+}
+
+var value = myfunction();
