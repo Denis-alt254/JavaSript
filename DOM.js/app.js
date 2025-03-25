d3fe969d113b9456eb13466979d793589f23802c
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-03-25 16:07:57 +0300

    DOM in html and conditions in js

---
+++ DOM.js/app.js
@@ -0,0 +1,1 @@
+document.getElementById("mypage").innerHTML = "New Page";
---
+++ conditions.js
@@ -0,0 +1,11 @@
+temperature = 30;
+
+if (temperature < 20) {
+    console.log("It's very cold outside");
+}
+else if (temperature < 30) {
+    console.log("The weather is moderate");
+}
+else {
+    console.log("It's very hot outside");
+}
