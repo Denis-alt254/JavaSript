a7730477b4a0afa1177169f3c6fbcbda9931c464
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-03-26 17:41:42 +0300

    new ones

--- DOM.js/app.js
+++ DOM.js/app.js
@@ -1,1 +1,5 @@
 document.getElementById("mypage").innerHTML = "New Page";
+document.getElementById("myBtn").onclick = displayDate;
+    function displayDate() {
+        document.getElementById("mypage").innerHTML = Date();
+    }      
