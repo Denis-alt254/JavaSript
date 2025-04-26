0800980896d953df13f88aba43018e83635d42d9
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-04-26 21:48:52 +0300

    updated

--- DOM.js/app.js
+++ DOM.js/app.js
@@ -2,4 +2,12 @@ document.getElementById("mypage").innerHTML = "New Page";
 document.getElementById("myBtn").onclick = displayDate;
     function displayDate() {
         document.getElementById("mypage").innerHTML = Date();
-    }      
+    } 
+myBtn.addEventListener("click", myfunction)
+    function myfunction(){
+        alert("Hello world!")
+    }
+mouse.addEventListener("mouseover", mysecondfunction) 
+    function mysecondfunction(){
+        alert("Click It")
+    } 
