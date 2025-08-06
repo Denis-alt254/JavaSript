47bd73cab3affc23c8369169b89ad59869d7609b
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-08-06 09:52:20 +0300

    finding the maximum number in an array of numbers

---
+++ FindMaxMin.js
@@ -0,0 +1,17 @@
+const arr = [23, 24, 54, 12, 32];
+var j = arr[0]
+var i = 1
+var k = arr[i]
+var max
+
+for (i; i <= arr.length-1; i++){
+    k = arr[i]
+}
+
+if(j > k){
+    max = j
+}else{
+    max = k
+}
+
+console.log("max number: ", max)
